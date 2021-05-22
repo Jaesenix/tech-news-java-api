1a7a170d1daff7bc8dbab9ea3caeed58e0bfc945
Janella Senica <janella.senica@gmail.com>
2021-05-21 23:13:54 -0500

    lesson 2

---
+++ tech-news-java-api/src/main/java/com/HelloWorld/src/VariableTypes.java
@@ -0,0 +1,15 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        int a = 8;
+        double b = 5.0;
+        String c = "dog";
+
+        double sum = a + b;
+
+        String concat = a + c;
+
+        System.out.println(sum);
+
+        System.out.println(concat);
+    }
+}
