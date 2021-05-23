2390c7042f4e148d1d7d45fcfd9eee147166ffb9
Janella Senica <janella.senica@gmail.com>
2021-05-23 13:25:55 -0500

    lesson 3 complete

--- tech-news-java-api/src/main/java/com/technews/src/com/technews/repository/CommentRepository.java
+++ tech-news-java-api/src/main/java/com/technews/src/com/technews/repository/CommentRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-3
+
 @Repository
 public interface CommentRepository extends  JpaRepository<Comment, Integer> {
 
